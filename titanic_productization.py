ef14d0b2dcccc38c295f9e87b4a5a0dbd9fe29c8
sergii.volodko <svolodko@8thlight.com>
2022-02-04 01:04:16 +0000

    productization script: group train and test transformations

--- titanic_productization.py
+++ titanic_productization.py
@@ -13,57 +13,54 @@ def run_all():
 
     # Note that where applicable we perform operations on both training and testing datasets together to stay consistent.
 
-    train_df = train_df.drop(['Ticket', 'Cabin', 'PassengerId'], axis=1)
-    test_df = test_df.drop(['Ticket', 'Cabin'], axis=1)
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5, np.nan: 0}
+    sex_mapping = {'female': 1, 'male': 0}
+    ports_mapping = {'S': 0, 'C': 1, 'Q': 2}
 
     train_df = add_title_from_name(train_df)
-    test_df = add_title_from_name(test_df)
-
     train_df = classify_rare_titles(train_df)
-    test_df = classify_rare_titles(test_df)
-
-    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5, np.nan: 0}
     train_df = convert_to_ordinal(train_df, "Title", title_mapping)
-    test_df = convert_to_ordinal(test_df, "Title", title_mapping)
 
-    train_df = train_df.drop(['Name'], axis=1)
-    test_df = test_df.drop(['Name'], axis=1)
-
-    sex_mapping = {'female': 1, 'male': 0}
     train_df = convert_to_ordinal(train_df, 'Sex', sex_mapping)
-    test_df = convert_to_ordinal(test_df, 'Sex', sex_mapping)
 
     age_suggestions = make_age_suggestions_matrix(train_df)
     train_df = fill_missing_age(train_df, age_suggestions)
+    train_df = convert_age_to_ordinal(train_df)
+
+    train_df = add_familysize_from_sibsp_and_parch(train_df)
+    train_df = add_isalone_from_familysize(train_df)
+
+    train_df = add_age_x_class(train_df)
+
+    train_df = fill_missing_embarked(train_df)
+    train_df = convert_to_ordinal(train_df, 'Embarked', ports_mapping)
+    train_df = convert_fare_to_ordinal(train_df)
+
+    train_df = train_df.drop(['Ticket', 'Cabin', 'PassengerId', 'Name', 'Parch', 'SibSp', 'FamilySize'], axis=1)
+
+    # Test
+    test_df = add_title_from_name(test_df)
+    test_df = classify_rare_titles(test_df)
+    test_df = convert_to_ordinal(test_df, "Title", title_mapping)
+
+    test_df = convert_to_ordinal(test_df, 'Sex', sex_mapping)
 
     age_suggestions = make_age_suggestions_matrix(test_df)
     test_df = fill_missing_age(test_df, age_suggestions)
-
-    train_df = convert_age_to_ordinal(train_df)
     test_df = convert_age_to_ordinal(test_df)
 
-    train_df = add_familysize_from_sibsp_and_parch(train_df)
     test_df = add_familysize_from_sibsp_and_parch(test_df)
-
-    train_df = add_isalone_from_familysize(train_df)
     test_df = add_isalone_from_familysize(test_df)
 
-    train_df = train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
-    test_df = test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
-
-    train_df = add_age_x_class(train_df)
     test_df = add_age_x_class(test_df)
 
-    train_df = fill_missing_embarked(train_df)
     test_df = fill_missing_embarked(test_df)
-
-    ports_mapping = {'S': 0, 'C': 1, 'Q': 2}
-    train_df = convert_to_ordinal(train_df, 'Embarked', ports_mapping)
     test_df = convert_to_ordinal(test_df, 'Embarked', ports_mapping)
 
     test_df = fill_missing_fare(test_df)
-
-    train_df = convert_fare_to_ordinal(train_df)
     test_df = convert_fare_to_ordinal(test_df)
+
+    test_df = test_df.drop(['Ticket', 'Cabin', 'Name', 'Parch', 'SibSp', 'FamilySize'], axis=1)
+
 
     return train_df, test_df
